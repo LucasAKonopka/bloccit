15c88a1295571228712b30c2a9370f5f6d7b71da
Lucas Konopka <lucasakonopka@gmail.com>
2015-11-02 05:32:33 +0000

    Checkpoint 39 completed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,8 @@ include RandomData
 
 RSpec.describe Comment, type: :model do
   let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:post) {topic.posts.create!(title: "New Post Title", body: "New Post Body")}
+  let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:post) {topic.posts.create!(title: "New Post Title", body: "New Post Body was too short" , user: user)}
   let(:comment) {Comment.create!(body: 'Comment body', post: post)}
   
   describe "attributes" do
