a808d6e898886471105c010d1e9c8f8276e621be
Lucas Konopka <lucasakonopka@gmail.com>
2015-12-16 04:31:58 +0000

    checkpoint46 complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,6 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   has_one :rating, as: :rateable
+  
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
