4418d42decfcc04394336d3541e3ef9a99d4552c
Lucas Konopka <lucasakonopka@gmail.com>
2015-12-16 04:58:27 +0000

    assignmetn46 complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,5 +5,10 @@ class Topic < ActiveRecord::Base
   has_many :labels, through: :labelings
   has_one :rating, as: :rateable
   
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  
+  scope :visible_to, -> (user){ user ? all : publically_viewable }
+  scope :publically_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
+ 
+  
 end
